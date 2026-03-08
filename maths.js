b76ea4b3738d49e4fa8bcc45d4ef4c29acc3d794
68907 <68907@etu.he2b.be>
2026-03-08 20:45:48 +0100

    JavaScript

---
+++ maths.js
@@ -0,0 +1,19 @@
+let x = 6;
+let y = 2;
+let z ;
+
+//z= Math.round(x);
+//z = Math.floor(x);
+//z = Math.ceil(y);
+//z = Math.trunc(x);
+//z = Math.pow(x,y);
+//z = Math.sqrt(x);
+//z = Math.cos(x);
+//z = Math.sin(x);
+//z = Math.tan(x);
+//z = Math.abs(-5);
+//z = Math.sign(-5);
+
+let max = Math.max(x,y);
+
+console.log(max);
